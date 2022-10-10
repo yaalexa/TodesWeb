48b90db5ed0e06eb8be93fb7a7d10affc2212e3f
harold <hehurtado9@misena.edu.co>
2022-10-10 14:18:45 -0500

    login ok

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,63 @@
 <template>
-    <div class="primero">
-        <div class="segundo">
-            <b-container class="formcontent" fluid>
-                <div class="fadein">
-                    <h1>Iniciar Sesion</h1>
-                    <img src="../assets/lgtb.png" alt="" class="icon">
-                </div>
 
-          <div class ="class">
-            <b-row class="my-1">
-              <b-col sm="1">
-                <label>Email</label>
-              </b-col>
-              <b-col sm="12">
-                <b-form-input id="" type="email"></b-form-input>
-              </b-col>
-            </b-row>
-  
-            <b-row class="my-1">
-              <b-col sm="1">
-                <label>Contraseña</label>
-              </b-col>
-              <b-col sm="12">
-                <b-form-input id="" type="password"></b-form-input>
-              </b-col>
-            </b-row>
-            <div class="link">
-              <b-link href="">¿Olvidaste tu contraseña?</b-link>
+<section class="vh-100" style="background-color: #00ADB5;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card" style="border-radius: 1rem;">
+          <div class="row g-0">
+            <div class="col-md-3 col-lg-5 d-none d-md-block">
+              <img src="../assets/lgtb.png"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
             </div>
-  
-            <b-button @click="Ingresar()" variant="outline-primary">ENTRAR</b-button>
-            
-         
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+
+                <form>
+
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                    <span class="h1 fw-bold mb-0">TODES</span>
+                  </div>
+
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Iniciar sesión en su cuenta</h5>
+
+                  <div class="form-outline mb-4">
+                    <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example17">Email</label>
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example27">Password</label>
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button @click="Ingresar()" class="btn btn-success btn-lg btn-block" type="button">Login</button>
+                  </div>
+
+                  <a class="small text-muted" href="#!">¿Se te olvidó tu contraseña?</a>
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">¿No tienes una cuenta? <a href="#!"
+                      style="color: #393f81;">
+Registrarse aquí</a></p>
+                 
+                </form>
+
+              </div>
             </div>
-          <div class="link">
-            <b-link href="">¿No tienes cuenta? Registrate</b-link>
           </div>
-            </b-container>
         </div>
+      </div>
     </div>
+  </div>
+</section>
 
+
+
+
+
+<!--++++++++++++++++++++++++++++++++++++-->
+    
 
 
 </template>
